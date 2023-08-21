7fe47f28b0a09771fac23cbc6562b71438a4ff57
hellun205 <hellun515@gmail.com>
2023-08-22 00:29:08 +0900

    map manger and controller
    player start position & death position
    
    implement #9

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -1,7 +1,36 @@
-﻿namespace Map
+﻿using System;
+using Managers;
+
+namespace Map
 {
-  public class MapManager
+  public class MapManager : MonoBehaivorSingleton<MapManager>
   {
-    
+    public MapController Controller { get; private set; }
+    public Room currentRoom { get; private set; }
+
+    private void Start()
+    {
+      OnSceneChanged();
+    }
+
+    public void OnSceneChanged()
+    {
+      Controller = FindObjectOfType<MapController>();
+      MoveTo(Controller.mainRoom);
+    }
+
+    public void MoveTo(string roomName, string link = "")
+    {
+      var room = Controller.rooms.Find(r => r.name == roomName);
+
+      if (room is not null)
+      {
+        currentRoom = room;
+        GameManager.Player.transform.position = room.startPosition.position;
+      }
+      else
+
+        throw new Exception($"invalid room name: {roomName}");
+    }
   }
 }
---
+++ Assets/Scripts/Map/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Map
+{
+  public class Room : MonoBehaviour
+  {
+    public Transform startPosition;
+
+    private void Awake()
+    {
+      startPosition = transform.Find("@start-pos");
+    }
+  }
+}
