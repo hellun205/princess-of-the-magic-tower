22b760012eda6af99f2a05e8cc6468e406ad81c2
hellun205 <hellun515@gmail.com>
2023-08-24 20:50:17 +0900

    remove smooth change

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Game;
 using Managers;
 using Map;
+using Scene;
 using UnityEngine;
 
 namespace Player
@@ -17,6 +18,12 @@ namespace Player
     {
       theCanvasManager.Death();
       transform.position = GameManager.Map.currentRoom.startPosition.position;
+    }
+
+    private void Update() {
+      if(Input.GetKeyDown(KeyCode.F6)) {
+        GameManager.Scene.Load("Test", new TransitionOption(Transitions.FADEOUT, 2), new TransitionOption(Transitions.FADEIN, 2), slowly:true);
+      }
     }
   }
 }
--- Assets/Scripts/Scene/SceneController.cs
+++ Assets/Scripts/Scene/SceneController.cs
@@ -30,13 +30,13 @@ namespace Scene
 
       yield return new WaitForSecondsRealtime(outAnimation.delay);
 
-      if (slowly) 
-        crt = StartCoroutine(ChangeSmooth(() => Time.timeScale, (v) => Time.timeScale = v, 0f, 2f, Mathf.Lerp, Mathf.Approximately));
+      //if (slowly) 
+      //  crt = StartCoroutine(ChangeSmooth(() => Time.timeScale, (v) => Time.timeScale = v, 0f, 2f, Mathf.Lerp, Mathf.Approximately));
       
       GameManager.Transition.Play(outAnimation.type, outAnimation.speed);
 
       yield return new WaitForSecondsRealtime(outAnimation.speed);
-      yield return new WaitUntil(() => load.isDone);
+      yield return new WaitUntil(() => load.progress >= 0.9f);
 
       if (slowly && crt != null)
         StopCoroutine(crt);
@@ -44,13 +44,21 @@ namespace Scene
 
       yield return new WaitForSecondsRealtime(inAnimation.delay);
       GameManager.Transition.Play(inAnimation.type, inAnimation.speed);
-      if (slowly) 
-        crt = StartCoroutine(ChangeSmooth(() => Time.timeScale, (v) => Time.timeScale = v, 1f, 2f, Mathf.Lerp, Mathf.Approximately));
+      //if (slowly) 
+      //  crt = StartCoroutine(ChangeSmooth(() => Time.timeScale, (v) => Time.timeScale = v, 1f, 2f, Mathf.Lerp, Mathf.Approximately));
 
       yield return new WaitForSecondsRealtime(inAnimation.speed);
 
       isLoading = false;
       callback?.Invoke();
+    }
+
+    private IEnumerator TimeScaleSmooth(float value, float smoothing = 5f) {
+      while(Mathf.Approximately(Time.timeScale, value)) {
+        Time.timeScale = Mathf.Lerp(Time.timeScale, value, Time.deltaTime * smoothing);
+
+        yield return new WaitForEndOfFrame();
+      }
     }
 
 
