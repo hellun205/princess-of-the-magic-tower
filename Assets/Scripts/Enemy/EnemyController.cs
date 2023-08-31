2c4e19066cff68605e3ba71e70db803296fd7bf8
hellun205 <hellun515@gmail.com>
2023-08-31 20:41:06 +0900

    enemy in map

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Managers;
 using Pool;
 using UnityEngine;
@@ -10,9 +10,20 @@ namespace Enemy
     public int maxHp;
     public int curHp;
 
+    public string map;
+
+    public void SetMap(string value) {
+      map = value;
+      GameManager.Map.Find(map).AddEnemy(pool.index);
+    }
+
     protected override void OnSummon()
     {
       curHp = maxHp;
+    }
+
+    protected override void OnKill() {
+      GameManager.Map.Find(map).RemoveEnemy(pool.index);
     }
 
     public void Hit(int damage)
