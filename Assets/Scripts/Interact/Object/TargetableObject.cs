d2d2f8b60eda063958212485e1834121186f0b24
yellow sky <hellun515@gmail.com>
2023-10-20 01:05:09 +0900

    add effect assets / apply hit effects

--- Assets/Scripts/Interact/Object/TargetableObject.cs
+++ Assets/Scripts/Interact/Object/TargetableObject.cs
@@ -1,4 +1,5 @@
 using System;
+using Managers;
 using UnityEngine;
 
 namespace Interact.Object
@@ -16,6 +17,7 @@ namespace Interact.Object
     protected override void OnInteract(GameObject caster)
     {
       if (!caster.TryGetComponent<AttackableObject>(out var ao)) return;
+      GameManager.Pool.Summon("effect/hit", transform.position);
       curHp = Math.Max(0, curHp - ao.damage);
       if (curHp == 0) onDead?.Invoke();
     }
